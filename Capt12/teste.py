6bb7bd606599e9c4f9099403c6ccad4fd20a64ce
Gabriel de Freitas Leite <gabriel.dfleite@gmail.com>
2022-11-23 16:53:16 -0300

    controle de bytes

--- Capt12/teste.py
+++ Capt12/teste.py
@@ -5,8 +5,11 @@ res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
 
 dataPath = Path('C:/Users/Gabriel/Desktop/Códigos/Git/Aprendendo_Python/Capt12')
 
-file = open(dataPath/"Romeu.txt",'w')
+res.raise_for_status()
 
-file.write(res.text[:100])
+file = open(dataPath/"Romeu.txt",'wb')
+
+for chunk in res.iter_content(0):
+        file.write(chunk)
 
 file.close()
