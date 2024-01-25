326a5a764bf7681d91d275a0062e1487ad4bdb51
Gabriel de Freitas Leite <gabriel.dfleite@gmail.com>
2024-01-25 17:21:31 -0300

    Messing with data

--- ML/script.zsh
+++ ML/script.zsh
@@ -3,10 +3,10 @@
 export ML_PATH="$HOME/Documents/Aprendendo_Python/ML"
 python3 -m pip install -U virtualenv # install virtualenv
 python3 -m virtualenv my_env # create virtualenv
+cd $ML_PATH
 
 # RUN
 
-cd $ML_PATH
 source my_env/bin/activate # activate virtualenv
 deactivate # deactivate virtualenv
 
@@ -19,5 +19,6 @@ python3 -m ipykernel install --user --name=python3 # install kernel for jupyter 
 # RUN
 
 jupyter notebook # run jupyter notebook
+control + C # stop jupyter notebook
 
 http://localhost:8888/  # open in browser
